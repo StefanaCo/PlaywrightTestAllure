6c88843ba93def0b7faaf8a14d9212c4bc8498df
Stefana <stefana.coman@eggs.de>
2024-08-06 13:08:03 +0300

    added github actions

--- playwright.config.js
+++ playwright.config.js
@@ -1,3 +1,4 @@
+const {devices} = require("@playwright/test");
 require('dotenv').config();
 
 const config = {
@@ -11,8 +12,10 @@ const config = {
     /*Shared settings for all the projects below. See https://playwright.dev/doc */
     use: {
         browserName: 'chromium',
+        use: {...devices['Desktop Chrome']},
         headless: true,
         screenshot: 'on',
+        video: 'on',
         trace: 'on',
     }
 };
